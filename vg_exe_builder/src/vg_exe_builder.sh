ee52b069b0a7c77eb59ef8d497dd4e1c50d646e7
Mike Lin <mlin@mlin.net>
2015-08-31 23:30:00 -0700

    vg_exe_builder types

--- vg_exe_builder/src/vg_exe_builder.sh
+++ vg_exe_builder/src/vg_exe_builder.sh
@@ -9,7 +9,8 @@ main() {
     sudo add-apt-repository -y ppa:kalakris/cmake
     sudo apt-get -qq update
     sudo apt-get install -y -qq gcc-4.9 g++-4.9 cmake
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 100 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 100 \
+                             --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
 
     # clone vg git repository
     git clone -n "$git_url" vg
@@ -25,6 +26,7 @@ main() {
     make test
 
     # upload the exe
-    vg_exe=$(gzip -c vg | dx upload --destination "vg-exe-${GIT_REVISION}.gz" --brief -)
+    vg_exe=$(gzip -c vg | dx upload --destination "vg-exe-${GIT_REVISION}.gz" --type vg_exe \
+                                    --property "git_revision=${GIT_REVISION}" --brief -)
     dx-jobutil-add-output vg_exe "$vg_exe" --class=file
 }
