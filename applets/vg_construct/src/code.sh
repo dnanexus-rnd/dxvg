a0ffcf1f411c3ca87397367325d1f0cf7472723f
Mike Lin <mlin@dnanexus.com>
2016-04-02 01:49:31 +0000

    distribute construct & index across multiple workers

--- applets/vg_construct/src/code.sh
+++ applets/vg_construct/src/code.sh
@@ -3,6 +3,28 @@
 main() {
     set -ex -o pipefail
 
+    # launch subjob to construct graph for each contig
+    jbors=""
+    for reference_contig in "${reference_contigs[@]}"; do
+        process_job=$(dx-jobutil-new-job process --name "construct $reference_contig" \
+                                                 -i "construct_options=${construct_options}" \
+                                                 -i "reference_contig=${reference_contig}" \
+                                                 -i "reference_genome=${reference_genome}" \
+                                                 -i "reference_variants=${reference_variants}")
+        jbors="$jbors -i vg:array:jobref=${process_job}:vg"
+    done
+
+    # schedule postprocessing job
+    if [ -z "$output_name" ]; then
+        output_name="$reference_genome_prefix"
+    fi
+    postprocess_job=$(dx-jobutil-new-job postprocess -i "output_name=${output_name}" $jbors)
+    dx-jobutil-add-output vg_tar "${postprocess_job}:vg_tar" --class=jobref
+}
+
+process() {
+    set -ex -o pipefail
+
     # fetch reference data
     dx cat "$reference_genome" | zcat > reference_genome.fa &
     dx download "$reference_variants" -o variants.vcf.gz
@@ -10,17 +32,27 @@ main() {
     wait
 
     # construct graph
+    vg construct -R "$reference_contig" -r reference_genome.fa -v variants.vcf.gz -t $(nproc) $construct_options > "${reference_contig}.vg"
+
+    # output to temp space
+    dx-jobutil-add-output vg --class=file \
+        $(dx upload --brief "${reference_contig}.vg")
+}
+
+postprocess() {
+    set -ex -o pipefail
+
     mkdir vg
-    printf '%s\n' "${reference_contigs[@]}" | \
-        xargs -i bash -ex -c "vg construct -R {} -r reference_genome.fa -v variants.vcf.gz -t $(nproc) $construct_options > vg/{}.vg"
-
+    pushd vg
+    # stage graphs from temp space
+    for vg_i in "${vg[@]}"; do
+        dx download "$vg_i"
+    done
     # rewrite IDs
-    vg ids -j $(printf 'vg/%s.vg\n' "${reference_contigs[@]}")
+    vg ids -j $(ls -1 *.vg)
+    popd
 
-    # tar everything up and output
-    if [ -z "$output_name" ]; then
-        output_name="$reference_genome_prefix"
-    fi
+    # tar up and output
     vg_tar=$(tar cv vg | dx upload --destination "${output_name}.vg.tar" --type vg_tar --brief -)
     dx-jobutil-add-output vg_tar "$vg_tar" --class=file
 }
--- applets/vg_index/src/code.sh
+++ applets/vg_index/src/code.sh
@@ -3,6 +3,38 @@
 main() {
     set -ex -o pipefail
 
+    # launch xg build
+    xgjob=$(dx-jobutil-new-job xg -i "vg_tar=${vg_tar}")
+
+    # unpack vg tar
+    dx cat "$vg_tar" | tar vx
+    pushd vg
+
+    # launch kmers jobs
+    mkdir -p /tmp/dxvg
+    jbors=""
+    for vgfn in $(ls -1 *.vg); do
+        vg_i=$(dx upload --brief "$vgfn")
+        kmers_job=$(dx-jobutil-new-job kmers --name "$vgfn kmers" -i "vg=${vg_i}" \
+                                             -i "prune_complex_options=${prune_complex_options}" \
+                                             -i "prune_subgraphs_options=${prune_subgraphs_options}" \
+                                             -i "kmers_options=${kmers_options}")
+        jbors="$jbors -i kmers:array:jobref=${kmers_job}:kmers"
+    done
+
+    # schedule gcsa job
+    gcsajob=$(dx-jobutil-new-job gcsa -i "vg_tar=${vg_tar}" -i "vg_tar_prefix=${vg_tar_prefix}" \
+                                      -i xgjob:string=${xgjob} \
+                                      -i "prune_complex_options=${prune_complex_options}" \
+                                      -i "prune_subgraphs_options=${prune_subgraphs_options}" \
+                                      -i "kmers_options=${kmers_options}" -i "gcsa_options=${gcsa_options}" \
+                                      $jbors)
+    dx-jobutil-add-output vg_indexed_tar --class=jobref "$gcsajob:vg_indexed_tar" 
+}
+
+xg() {
+    set -ex -o pipefail
+
     # unpack vg tar
     dx cat "$vg_tar" | tar vx
     pushd vg
@@ -10,21 +42,45 @@ main() {
     # build xg index
     vg index -x index.xg $(ls -1 *.vg)
 
-    # build GCSA index
-    mkdir -p /tmp/dxvg
-    kmers=""
-    for vgfn in $(ls -1 *.vg); do
-        seqname=${vgfn%.vg}
-        kmers="$kmers -i /tmp/dxvg/${seqname}.kmers"
-        vg mod -p -t $(nproc) $prune_complex_options "$vgfn" | \
-            vg mod -S $prune_subgraphs_options - | \
-            vg kmers -gB -t $(nproc) -H 1000000000 -T 1000000001 $kmers_options - > "/tmp/dxvg/${seqname}.kmers"
+    # upload it to temp storage
+    dx-jobutil-add-output xg --class=file $(dx upload --brief index.xg)
+}
+
+kmers() {
+    set -ex -o pipefail
+
+    dx-jobutil-add-output kmers --class=file \
+        $(dx cat "$vg" \
+            | vg mod -p -t $(nproc) $prune_complex_options - \
+            | vg mod -S $prune_subgraphs_options - \
+            | vg kmers -gB -t $(nproc) -H 1000000000 -T 1000000001 $kmers_options - \
+            | dx upload --destination "${vg_name}.kmers" --brief -)
+}
+
+gcsa() {
+    set -ex -o pipefail
+
+    mkdir vg
+
+    # spawn background downloads of vg tar and xg index.
+    dx cat "$vg_tar" | tar vx & vgpid=$!
+    download_xg "$xgjob" & xgpid=$!
+
+    # download kmers
+    mkdir -p /tmp/kmers
+    cd /tmp/kmers
+    for kmers_i in "${kmers[@]}"; do
+        dx download "$kmers_i"
     done
-    ls -1sh /tmp/dxvg
-    vg index -g index.gcsa $gcsa_options $kmers
+    ls -1sh /tmp/kmers/*.kmers
+
+    # build GCSA index
+    cd /home/dnanexus
+    vg index -g vg/index.gcsa $gcsa_options $(printf " -i %s" $(find /tmp/kmers -name "*.kmers" -type f))
 
     # tar everything up and output
-    popd
+    wait $vgpid
+    wait $xgpid
     index_options_alnum=$(echo "${prune_complex_options}${prune_subgraphs_options}${kmers_options}${gcsa_options}" | tr -cd '[[:alnum:]]')
     vg_indexed_tar=$(tar cv vg | \
                        dx upload --destination "${vg_tar_prefix}.vg.index_${index_options_alnum}.tar" \
@@ -34,4 +90,19 @@ main() {
                          --property "gcsa_options=${gcsa_options}" \
                          --type vg_indexed_tar --brief -)
     dx-jobutil-add-output vg_indexed_tar "$vg_indexed_tar" --class=file
+}
+
+download_xg() {
+    set -ex -o pipefail
+
+    # download the xg index in the background of the GCSA-building job. This
+    # is a bit convoluted because we're trying to save overhead by not having
+    # either job formally depend on the other. We assume the GCSA index takes
+    # much longer to build than the xg index.
+
+    dx wait "$1"
+    xgdxfile=$(dx describe "$1" --json | jq '.output.xg')
+    xgdxfile=$(dx-jobutil-parse-link --no-project "$xgdxfile")
+    dx wait "$xgdxfile"
+    dx download "$xgdxfile" -o /home/dnanexus/vg/index.xg
 }
